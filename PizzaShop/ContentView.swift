5005e6b2307ecbcdb4b2d8769d1730ab17d154f6
Vitali Klopau <vitaliklopau@gmail.com>
2023-08-12 00:10:15 +0200

    adding a ternary operator and animation for the authorization screen

--- PizzaShop/ContentView.swift
+++ PizzaShop/ContentView.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var isAuth = true
+    
     @State private var email = ""
     @State private var password = ""
+    @State private var confirmPassword = ""
     
     var body: some View {
         
         VStack(spacing: 20) {
-            Text("Авторизация")
-                .padding()
+            Text(isAuth ? "Авторизация" : "Регистрация")
+                .padding(isAuth ? 16 : 24)
                 .padding(.horizontal, 30)
                 .font(.title2.bold())
                 .background(Color("whiteAlpha"))
-                .cornerRadius(30)
+                .cornerRadius(isAuth ? 30 : 60)
             
             VStack {
                 TextField("Введите Email", text: $email)
@@ -30,17 +33,30 @@ struct ContentView: View {
                     .padding(8)
                     .padding(.horizontal, 12)
                 
+                //MARK: Password field
+                
                 SecureField("Введите Пароль", text: $password)
                     .padding()
                     .background(Color("whiteAlpha"))
                     .cornerRadius(12)
                     .padding(8)
                     .padding(.horizontal, 12)
-                    
+                
+                if isAuth {
+                    SecureField("Повторите Пароль", text: $password)
+                        .padding()
+                        .background(Color("whiteAlpha"))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                }
+                
+               //MARK: Button Login
+                
                 Button {
                     print("Авторизация")
                 } label: {
-                    Text("Войти")
+                    Text(isAuth ? "Войти" : "Создать аккаунт")
                         .padding()
                         .frame(maxWidth: .infinity)
                         .background(LinearGradient(colors: [Color("yellow"), Color("orange")], startPoint: .leading, endPoint: .trailing))
@@ -51,10 +67,14 @@ struct ContentView: View {
                         .foregroundColor(Color("darkbrown"))
                         
                 }
+                
+                //MARK: Button not with us
+                
                 Button {
+                    isAuth.toggle()
                     print("Еще не с нами ?")
                 } label: {
-                    Text("Еще не с нами ?")
+                    Text(isAuth ?  "Еще не с нами ?" : "Уже есть аккаунт")
                         .padding(.horizontal)
                         .frame(maxWidth: .infinity)
                         .cornerRadius(12)
@@ -70,12 +90,15 @@ struct ContentView: View {
             .padding(.top, 16)
             .background(Color("whiteAlpha"))
             .cornerRadius(24)
-            .padding(30)
+            .padding(isAuth ? 30 : 12)
            
             
             
         }.frame(maxWidth: .infinity,maxHeight: .infinity)
-            .background(Image("bg").ignoresSafeArea())
+            .background(Image("bg").ignoresSafeArea()
+                .blur(radius: isAuth ? 0 : 6))
+            .animation(Animation.easeInOut(duration: 0.3), value: isAuth)
+            
     }
 }
 
